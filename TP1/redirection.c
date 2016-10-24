c825e31517d1f1f5c55a223f054455fcfeba5ca0
Norips <raphdruon@gmail.com>
2016-10-24 13:21:41 +0200

    Add comment and close

--- TP1/redirection.c
+++ TP1/redirection.c
@@ -20,14 +20,15 @@ void print() {
     printf("Test 3 \n");
     printf("Test 4 \n");
 }
+
 void rediriger_vers(void (*f) (void), char* file) {
-    int back = dup(1);
-    int fd = open(file,O_CREAT | O_WRONLY | O_TRUNC,0666);
-    verif((fd == -1), "open file");
-    verif(dup2(fd,1) == -1,"dup2");
-    f();
-    verif(dup2(back,1) == -1,"dup2");
-    
+    int back = dup(1); //On sauvegarde la sortie standard
+    int fd = open(file,O_CREAT | O_WRONLY | O_TRUNC,0666); //On ouvre/créer un fichier en ecriture
+    verif((fd == -1), "open file"); //On verifie l'ouverture
+    verif(dup2(fd,1) == -1,"dup2"); //On verifie la duplication de fd dans 1, quand on écrira dans la sortie standard(1) cela ecrira sur le fichier(fd)
+    f(); //On appel la fonction passé en paramètre
+    verif(dup2(back,1) == -1,"dup2"); //On restaure la sortie standard sauvegardé
+    close(fd);
 }
 int main(int argc, char** argv) {
     printf("Sortie standard 1\n");
