5d3a69879bb251b0d9bfda70df41bc8a0f584737
Norips <raphdruon@gmail.com>
2016-11-08 11:33:55 +0100

    Initial commit

---
+++ TP5/calcul-en-parallele.c
@@ -0,0 +1,34 @@
+#include <math.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX  10000000
+double resultat[MAX];
+int nb_threads;
+
+void *Calculate(void *p) {
+    int n = *((int*)p);
+    for(int i = n*MAX/nb_threads; i < (n+1)*MAX/nb_threads-1;i++){
+        resultat[i] = acos(cos((double) i));
+    }
+    return NULL;
+}
+int main(int argc, char *argv[]) {
+    nb_threads = atoi(argv[1]);
+
+    pthread_t *tab_thread = malloc(nb_threads * sizeof (pthread_t*));
+    
+    for (int i = 0; i < nb_threads; i++) {
+        tab_thread[i] = malloc(sizeof (pthread_t));
+        int *n = malloc(sizeof(int));
+        *n = i;
+        pthread_create(&tab_thread[i], NULL, Calculate, n);
+    }
+
+    for (int i = 0; i < nb_threads; i++) {
+        pthread_join(tab_thread[i], NULL);
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ TP5/hello-goodbye.c
@@ -0,0 +1,37 @@
+/* 
+ * File:   hello-goodbye.c
+ * Author: rdruon
+ *
+ * Created on 8 novembre 2016, 11:07
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "../TP1/tool/tools.h"
+
+void *HelloGoodbye(void *p) {
+    printf("%p: bonjour\n", pthread_self());
+    sleep(1);
+    printf("%p: a bientot\n", pthread_self());
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    checkParam(argc,1);
+    int k = atoi(argv[1]);
+    pthread_t *tab_thread = malloc(k * sizeof(pthread_t*));
+    for(int i = 0; i < k; i++){
+        tab_thread[i] = malloc(sizeof(pthread_t));
+        pthread_create(&tab_thread[i],NULL,HelloGoodbye,NULL);
+    }
+    
+    for(int i = 0; i < k; i++){
+        pthread_join(tab_thread[i],NULL);
+    }
+
+    return EXIT_SUCCESS;
+}
