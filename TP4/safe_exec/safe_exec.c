e586e200b0da6cb71dbacd8256146ffe0992bd37
Norips <raphdruon@gmail.com>
2016-11-15 11:13:18 +0100

    Correct

--- TP4/safe_exec/safe_exec.c
+++ TP4/safe_exec/safe_exec.c
@@ -4,21 +4,26 @@
 
 #include "safe_exec.h"
 
-sigjmp_buf buf;
+sigjmp_buf *current;
 
 void routine(int signo) {
-    siglongjmp(buf,1);
+    siglongjmp(*current,1);
 }
 int essayer(void (*fun)(void),int sig){
-    if(sigsetjmp(buf,1)==0) {
+    sigjmp_buf new, *old;
+    old = current;
+    current = &new;
+    if(sigsetjmp(current,1)==0) {
         struct sigaction s;
         s.sa_handler = routine;
         sigemptyset(&s.sa_mask);
         s.sa_flags=0;
         sigaction(sig,&s,NULL);
         fun();
+        current = old;
         return 0;
     } else {
+        current = old;
         return -1;
     }
 }
--- TP4/safe_exec/safe_exec_test.c
+++ TP4/safe_exec/safe_exec_test.c
@@ -20,17 +20,19 @@ void g() {
 void f() {
     int r;
     r = essayer(g,SIGSEGV);
+    int *x = NULL;
+    *x = 12;
     if(r) {
-        printf("Erreur durant l'execution.\n");
+        printf("Erreur durant l'execution de g\n");
     } else {
-        printf("L'execution de f s'est bien déroulé.\n");
+        printf("L'execution de g s'est bien déroulé.\n");
     }
 }
 int main(int argc, char** argv) {
     int r;
     r = essayer(f,SIGSEGV);
     if(r) {
-        printf("Erreur durant l'execution.\n");
+        printf("Erreur durant l'execution de f.\n");
     } else {
         printf("L'execution de f s'est bien déroulé.\n");
     }
