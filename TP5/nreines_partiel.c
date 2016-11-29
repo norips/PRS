c3198bba9788a60431f2279cd4db9131fefb25f3
Norips <raphdruon@gmail.com>
2016-11-29 10:29:30 +0100

    ?

--- TP5/nreines_partiel.c
+++ TP5/nreines_partiel.c
@@ -11,88 +11,76 @@
 typedef bool echiquier[MAX][MAX];
 
 
-static bool ok (int n, int ligne, int colonne, echiquier e);
+static bool ok(int n, int ligne, int colonne, echiquier e);
 
-
-
-
-
-void 
-nreines (int n,int ligne, echiquier e, int *cpt)
-{
-  for (int col = 0; col < n; col++)
-    if (ok (n, ligne, col, e))
-    {
-      if (ligne == n - 1)
-	(*cpt)++;
-      else
-      {
-          bool present = false;
-          if(e[ligne][col]) {
-              present = true;
-          }
-	e[ligne][col] = true;
-	nreines (n, ligne + 1, e, cpt);
-        if(!present)
-            e[ligne][col] = false;
-      }
-    }
+void
+nreines(int n, int ligne, echiquier e, int *cpt) {
+    for (int col = 0; col < n; col++)
+        if (ok(n, ligne, col, e)) {
+            if (ligne == n - 1)
+                (*cpt)++;
+            else {
+                bool present = false;
+                if (e[ligne][col]) {
+                    present = true;
+                }
+                e[ligne][col] = true;
+                nreines(n, ligne + 1, e, cpt);
+                if (!present)
+                    e[ligne][col] = false;
+            }
+        }
 }
-
 
 static bool
-ok (int n, int ligne, int colonne, echiquier e)
-{
-  int l, c;
-  for (l = 0; l < ligne; l++)
-    if (e[l][colonne])
-      return false;
+ok(int n, int ligne, int colonne, echiquier e) {
+    int l, c;
+    for (l = 0; l < ligne; l++)
+        if (e[l][colonne])
+            return false;
 
-  for (l = ligne - 1, c = colonne - 1; l >= 0 && c >= 0; l--, c--)
-    if (e[l][c])
-      return false;
+    for (l = ligne - 1, c = colonne - 1; l >= 0 && c >= 0; l--, c--)
+        if (e[l][c])
+            return false;
 
-  for (l = ligne - 1, c = colonne + 1; l >= 0 && c <= n; l--, c++)
-    if (e[l][c])
-      return false;
-  return true;
+    for (l = ligne - 1, c = colonne + 1; l >= 0 && c <= n; l--, c++)
+        if (e[l][c])
+            return false;
+    return true;
 }
 
-
-void 
-usage (char *s)
-{
-  fprintf (stderr, "%s entier entier", s);
-  exit (EXIT_FAILURE);
+void
+usage(char *s) {
+    fprintf(stderr, "%s entier entier", s);
+    exit(EXIT_FAILURE);
 }
 
 int
-main (int argc, char *argv[])
-{
-  echiquier e;
-  int cpt = 0;
-  char *endptr;
+main(int argc, char *argv[]) {
+    echiquier e;
+    int cpt = 0;
+    char *endptr;
 
-  if (argc < 3)
-    usage (argv[0]);
+    if (argc < 3)
+        usage(argv[0]);
 
-  int n = strtoul (argv[1], &endptr, 10);
-  if (*endptr != 0)
-    usage (argv[1]);
-  
-  int start = strtol (argv[2], &endptr, 10);
-  if (*endptr != 0)
-    usage (argv[1]);
+    int n = strtoul(argv[1], &endptr, 10);
+    if (*endptr != 0)
+        usage(argv[1]);
 
-  memset (e, 0, sizeof (e));
-  e[0][start%16] = true;
-  nreines (n, 0, e, &cpt);
-  printf("res : %d\n",cpt);
-  int file = open("/tmp/nreine",O_WRONLY | O_CREAT, 0666);
-  lseek(file,start*sizeof(int),SEEK_SET);
-  write(file,&cpt,sizeof(int));
+    int start = strtol(argv[2], &endptr, 10);
+    if (*endptr != 0)
+        usage(argv[1]);
 
-  return EXIT_SUCCESS;
+    memset(e, 0, sizeof (e));
+    e[0][start % 16] = true;
+    nreines(n, 0, e, &cpt);
+    printf("res : %d\n", cpt);
+    int file = open("/tmp/nreine", O_WRONLY | O_CREAT, 0666);
+    lseek(file, start * sizeof (int), SEEK_SET);
+    write(file, &cpt, sizeof (int));
+
+    return EXIT_SUCCESS;
 }
 
 
--- TP5/nreines_thread.c
+++ TP5/nreines_thread.c
@@ -4,81 +4,104 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <pthread.h>
 
 #define MAX 16
 
 typedef bool echiquier[MAX][MAX];
 
 
-static bool ok (int n, int ligne, int colonne, echiquier e);
+static bool ok(int n, int ligne, int colonne, echiquier e);
 
+typedef struct struct_param {
+    int n;
+    echiquier e;
+    int *cpt;
+    int current;
+} struct_param;
 
-
-
-
-void 
-nreines (int n, int ligne, echiquier e, int *cpt)
-{
-  for (int col = 0; col < n; col++)
-    if (ok (n, ligne, col, e))
-    {
-      if (ligne == n - 1)
-	(*cpt)++;
-      else
-      {
-	e[ligne][col] = true;
-	nreines (n, ligne + 1, e, cpt);
-	e[ligne][col] = false;
-      }
-    }
+void
+nreines(int n, int ligne, echiquier e, int *cpt) {
+    for (int col = 0; col < n; col++)
+        if (ok(n, ligne, col, e)) {
+            if (ligne == n - 1)
+                (*cpt)++;
+            else {
+                bool present = false;
+                if (e[ligne][col]) {
+                    present = true;
+                }
+                e[ligne][col] = true;
+                nreines(n, ligne + 1, e, cpt);
+                if (!present)
+                    e[ligne][col] = false;
+            }
+        }
 }
 
+void* threadFunc(void *n) {
+    struct_param *param = (struct_param*) n;
+    nreines(param->n, 0, param->e, param->cpt);
+    return NULL;
+}
 
 static bool
-ok (int n, int ligne, int colonne, echiquier e)
-{
-  int l, c;
-  for (l = 0; l < ligne; l++)
-    if (e[l][colonne])
-      return false;
+ok(int n, int ligne, int colonne, echiquier e) {
+    int l, c;
+    for (l = 0; l < ligne; l++)
+        if (e[l][colonne])
+            return false;
 
-  for (l = ligne - 1, c = colonne - 1; l >= 0 && c >= 0; l--, c--)
-    if (e[l][c])
-      return false;
+    for (l = ligne - 1, c = colonne - 1; l >= 0 && c >= 0; l--, c--)
+        if (e[l][c])
+            return false;
 
-  for (l = ligne - 1, c = colonne + 1; l >= 0 && c <= n; l--, c++)
-    if (e[l][c])
-      return false;
-  return true;
+    for (l = ligne - 1, c = colonne + 1; l >= 0 && c <= n; l--, c++)
+        if (e[l][c])
+            return false;
+    return true;
 }
 
-
-void 
-usage (char *s)
-{
-  fprintf (stderr, "%s entier", s);
-  exit (EXIT_FAILURE);
+void
+usage(char *s) {
+    fprintf(stderr, "%s entier", s);
+    exit(EXIT_FAILURE);
 }
 
 int
-main (int argc, char *argv[])
-{
-  echiquier e;
-  int cpt = 0;
-  char *endptr;
+main(int argc, char *argv[]) {
+    int cpt = 0;
+    char *endptr;
 
-  if (argc < 2)
-    usage (argv[0]);
+    if (argc < 2)
+        usage(argv[0]);
 
-  int n = strtoul (argv[1], &endptr, 10);
+    int n = strtoul(argv[1], &endptr, 10);
 
-  if (*endptr != 0)
-    usage (argv[1]);
+    if (*endptr != 0)
+        usage(argv[1]);
+    pthread_t tab_thread[n];
+    struct_param tab_param[n];
+    for (int i = 0; i < n; i++) {
+        tab_param[i].cpt = malloc(sizeof (int));
+        *tab_param[i].cpt = 0;
+        tab_param[i].n = n;
+        tab_param[i].current = i;
+        memset(tab_param[i].e, 0, sizeof (tab_param[i].e));
+        tab_param[i].e[0][i % 16] = true;
+    }
+    for (int i = 0; i < n; i++) {
+        pthread_create(tab_thread + i, NULL, threadFunc, tab_param + i);
+    }
 
-  memset (e, 0, sizeof (e));
-  nreines (n, 0, e, &cpt);
-  printf("%d\n",cpt);
+    for (int i = 0; i < n; i++) {
+        pthread_join(tab_thread[i], NULL);
+    }
+    for (int i = 0; i < n; i++) {
+        cpt+=*tab_param[i].cpt;
+    }
+    printf("%d\n", cpt);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
